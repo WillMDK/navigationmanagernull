8fe4adc77b57a150b3f80601d80ac572f1459be4
William Mortensen <wmo@dalux.com>
2025-02-27 12:16:06 +0100

    initial commit

---
+++ navigationmanagernull/navigationmanagernull/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace navigationmanagernull;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+    }
+
+    private void OnNavigateClicked(object sender, EventArgs e)
+    {
+        Navigation.PushModalAsync(new NavigationPage(new MainPage()),false);
+    }
+}
+
